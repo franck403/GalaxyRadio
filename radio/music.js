524a2b41c50d51eaf41bcd384455654da3e74572
virus gaming <79372051+franck403@users.noreply.github.com>
2022-09-24 12:32:46 +0000

    Start the radio page

---
+++ radio/music.js
@@ -0,0 +1,9 @@
+var x = document.getElementById("https://Fps2.listen2myradio.com:2199/listen.php?ip=109.169.23.17&port=8902&type=s1");
+
+function playAudio() {
+  x.play();
+}
+
+function pauseAudio() {
+  x.pause();
+}
